d793e7cf9779f51e95f9bd9a549864ecaf08518d
dfdx <faithlessfriend@gmail.com>
2015-06-11 02:39:50 +0300

    skeleton

--- src/groupby.jl
+++ src/groupby.jl
@@ -1,4 +1,6 @@
 
+using Docile
+
 type VarStats
     n_obs::Int64
     min::Float64
@@ -23,7 +25,7 @@ function updatestats!(vs::VarStats, value::Real)
     end
     if value > vs.max
         vs.max = value
-    end    
+    end
 end
 
 
@@ -44,6 +46,69 @@ function aggregate!{T<:Real}(agg::Aggregator,
 end
 
 
+type Index
+    names::Vector{Symbol}
+    lookup::Dict{Symbol, Int}
+    Index(names::Vector{Symbol}) =
+        Index(names, Dict([(n, i) for (i, n) in enumerate(names)]))
+end
+
+
+@doc """
+Create grouper that adjusts value to the nearest multiple of step.
+Usage:
+    grouper = stepgrouper(5)
+    grouper(75) ==> 75
+    grouper(78) ==> 80
+    grouper(80) ==> 80
+""" ->
+function stepgrouper(step::Float64)
+    function grouper(value)
+        round(value / step) * step
+    end
+    return grouper
+end
+
+
+type GroupBy
+    vindex::Index
+    gindex::Index
+    aindex::Index
+    agg::Aggregator
+end
+
+
+@doc "Object for incremental grouping by some vars and aggreating the others" ->
+function GroupBy(names::Vector{Symbol}, groupers::Dict{Symbol, Function})
+    gkeys = keys(groupers)
+    gvars = filter(x -> in(x, gkeys), names)
+    avars = filter(x -> !in(x, gkeys), names)
+    vindex = Index(names)
+    gindex = Index(gvars)
+    aindex = Index(avars)
+    return GroupBy(vindex, gindex, aindex, Aggregator())
+end
+
+function GroupBy(names::Vector{Symbol}, groupfields::Vector{Symbol})
+    # shortcut with all groupers set to identity()
+end
+
+function getgroup(grb::GroupBy, values::Vector{Float64})
+    # select needed vars
+    # run groupers on each of them
+end
+    
+
+function getagg(grb::GroupBy, values::Vector{Float64})
+    
+end
+
+function updategroup!(grb::GroupBy, values::Vector{Float64})
+    # get group
+    # get aggregation vars
+    # run aggregator
+end
+
 
 # usage:
 #  grb = GroupBy(names=[:a, :b, :c, :d],
@@ -51,4 +116,3 @@ end
 #  updategroup!(grb, [1, 74, 2, .33])
 #  grb2 = where(grb, ??)
 #  histogram(grb, :d)  ==> 1) find all groups; 2) get corresponding gkey values and n_obs from first VarStats
-
